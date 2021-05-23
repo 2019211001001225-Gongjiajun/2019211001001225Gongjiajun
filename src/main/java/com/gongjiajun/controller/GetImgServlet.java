e48658773cd7bf8b0712b607fab7c5acf561592c
2019211001001225Gongjiajun <3070293285@qq.com>
2021-05-23 19:55:17 +0800

    HW-Week 12 Commit

--- src/main/java/com/gongjiajun/controller/GetImgServlet.java
+++ src/main/java/com/gongjiajun/controller/GetImgServlet.java
@@ -1,31 +1,39 @@
 package com.gongjiajun.controller;
 
 import com.gongjiajun.dao.ProductDao;
-import com.gongjiajun.model.Product;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
 
-@WebServlet(name = "ProductListServlet", value = "/admin/productList")
-public class ProductListServlet extends HttpServlet {
+@WebServlet(name = "GetImgServlet", value = "/getImg")
+public class GetImgServlet extends HttpServlet {
     Connection con=null;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProductDao productDao=new ProductDao();
+        response.setContentType("text/html");
+        ProductDao dao=new ProductDao();
+        int id=0;
+        if(request.getParameter("id")!=null){
+            id=Integer.parseInt(request.getParameter("id"));
+
+        }
+        byte[] imgByte=new byte[0];
         try {
-            List<Product> productList=productDao.findAll(con);
-            request.setAttribute("productList",productList);
+            imgByte=dao.getPictureById(id,con);
+            if(imgByte!=null){
+                response.setContentType("image/gif");
+                OutputStream os=response.getOutputStream();
+                os.write(imgByte);
+                os.flush();
+            }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        String path="/WEB-INF/views/admin/productList.jsp";
-        request.getRequestDispatcher(path).forward(request,response);
-
 
     }
 
